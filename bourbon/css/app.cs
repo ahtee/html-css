7a43c3279cdce1fc777b77b635765cc9ff604c67
ahtee <ottebdrum@gmail.com>
2016-03-10 21:51:58 -0500

    Added all thre bourbon packages: Neat, Bitters, and started using the Refills functionality.

--- bourbon/css/app.cs
+++ bourbon/css/app.cs
@@ -1,32 +1,33 @@
 /*
-Error: Undefined variable: "$font-stack-system".
-        on line 6 of 0-plugins/base/_variables.scss
+Error: Invalid CSS after "$serif": expected selector or at-rule, was ": $georgia"
+        on line 7 of 0-plugins/base/_variables.scss
         from line 6 of 0-plugins/base/_base.scss
-        from line 4 of 0-plugins/plugins-dir.sass
+        from line 5 of 0-plugins/plugins-dir.sass
         from line 1 of app.sass
 
-1: @import '0-plugins/plugins-dir'
-2: @import '1-base/base-dir'
-3: @import '2-modules/modules-dir'
-4: @import '3-layouts/layouts-dir'
+2: $medium-screen: 600px;
+3: $large-screen: 900px;
+4: 
+5: // Typography
+6: $sans-serif: $monospace
+7: $serif: $georgia
+8: 
+9: $base-font-family: $font-stack-system;
+10: $heading-font-family: $base-font-family;
+11: 
+12: // Font Sizes
 
 Backtrace:
-0-plugins/base/_variables.scss:6
+0-plugins/base/_variables.scss:7
 0-plugins/base/_base.scss:6
-0-plugins/plugins-dir.sass:4
+0-plugins/plugins-dir.sass:5
 app.sass:1
-C:/RailsInstaller/Ruby2.2.0/lib/ruby/gems/2.2.0/gems/sass-3.4.21/lib/sass/script/tree/variable.rb:49:in `_perform'
-C:/RailsInstaller/Ruby2.2.0/lib/ruby/gems/2.2.0/gems/sass-3.4.21/lib/sass/script/tree/node.rb:50:in `perform'
-C:/RailsInstaller/Ruby2.2.0/lib/ruby/gems/2.2.0/gems/sass-3.4.21/lib/sass/tree/visitors/perform.rb:469:in `visit_variable'
-C:/RailsInstaller/Ruby2.2.0/lib/ruby/gems/2.2.0/gems/sass-3.4.21/lib/sass/tree/visitors/base.rb:36:in `visit'
-C:/RailsInstaller/Ruby2.2.0/lib/ruby/gems/2.2.0/gems/sass-3.4.21/lib/sass/tree/visitors/perform.rb:158:in `block in visit'
-C:/RailsInstaller/Ruby2.2.0/lib/ruby/gems/2.2.0/gems/sass-3.4.21/lib/sass/stack.rb:79:in `block in with_base'
-C:/RailsInstaller/Ruby2.2.0/lib/ruby/gems/2.2.0/gems/sass-3.4.21/lib/sass/stack.rb:115:in `with_frame'
-C:/RailsInstaller/Ruby2.2.0/lib/ruby/gems/2.2.0/gems/sass-3.4.21/lib/sass/stack.rb:79:in `with_base'
-C:/RailsInstaller/Ruby2.2.0/lib/ruby/gems/2.2.0/gems/sass-3.4.21/lib/sass/tree/visitors/perform.rb:158:in `visit'
-C:/RailsInstaller/Ruby2.2.0/lib/ruby/gems/2.2.0/gems/sass-3.4.21/lib/sass/tree/visitors/perform.rb:326:in `block (2 levels) in visit_import'
-C:/RailsInstaller/Ruby2.2.0/lib/ruby/gems/2.2.0/gems/sass-3.4.21/lib/sass/tree/visitors/perform.rb:326:in `map'
-C:/RailsInstaller/Ruby2.2.0/lib/ruby/gems/2.2.0/gems/sass-3.4.21/lib/sass/tree/visitors/perform.rb:326:in `block in visit_import'
+C:/RailsInstaller/Ruby2.2.0/lib/ruby/gems/2.2.0/gems/sass-3.4.21/lib/sass/scss/parser.rb:1179:in `expected'
+C:/RailsInstaller/Ruby2.2.0/lib/ruby/gems/2.2.0/gems/sass-3.4.21/lib/sass/scss/parser.rb:1115:in `expected'
+C:/RailsInstaller/Ruby2.2.0/lib/ruby/gems/2.2.0/gems/sass-3.4.21/lib/sass/scss/parser.rb:40:in `parse'
+C:/RailsInstaller/Ruby2.2.0/lib/ruby/gems/2.2.0/gems/sass-3.4.21/lib/sass/engine.rb:403:in `_to_tree'
+C:/RailsInstaller/Ruby2.2.0/lib/ruby/gems/2.2.0/gems/sass-3.4.21/lib/sass/engine.rb:309:in `to_tree'
+C:/RailsInstaller/Ruby2.2.0/lib/ruby/gems/2.2.0/gems/sass-3.4.21/lib/sass/tree/visitors/perform.rb:324:in `block in visit_import'
 C:/RailsInstaller/Ruby2.2.0/lib/ruby/gems/2.2.0/gems/sass-3.4.21/lib/sass/stack.rb:88:in `block in with_import'
 C:/RailsInstaller/Ruby2.2.0/lib/ruby/gems/2.2.0/gems/sass-3.4.21/lib/sass/stack.rb:115:in `with_frame'
 C:/RailsInstaller/Ruby2.2.0/lib/ruby/gems/2.2.0/gems/sass-3.4.21/lib/sass/stack.rb:88:in `with_import'
@@ -94,4 +95,4 @@ C:/RailsInstaller/Ruby2.2.0/lib/ruby/gems/2.2.0/gems/sass-3.4.21/vendor/listen/l
 body:before {
   white-space: pre;
   font-family: monospace;
-  content: "Error: Undefined variable: \"$font-stack-system\".\A         on line 6 of 0-plugins/base/_variables.scss\A         from line 6 of 0-plugins/base/_base.scss\A         from line 4 of 0-plugins/plugins-dir.sass\A         from line 1 of app.sass\A \A 1: @import '0-plugins/plugins-dir'\A 2: @import '1-base/base-dir'\A 3: @import '2-modules/modules-dir'\A 4: @import '3-layouts/layouts-dir'"; }
+  content: "Error: Invalid CSS after \"$serif\": expected selector or at-rule, was \": $georgia\"\A         on line 7 of 0-plugins/base/_variables.scss\A         from line 6 of 0-plugins/base/_base.scss\A         from line 5 of 0-plugins/plugins-dir.sass\A         from line 1 of app.sass\A \A 2: $medium-screen: 600px;\A 3: $large-screen: 900px;\A 4: \A 5: // Typography\A 6: $sans-serif: $monospace\A 7: $serif: $georgia\A 8: \A 9: $base-font-family: $font-stack-system;\A 10: $heading-font-family: $base-font-family;\A 11: \A 12: // Font Sizes"; }
